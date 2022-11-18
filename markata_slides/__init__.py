9171f02108bac8568c4fa3133b14bff3589fb22b
autobump <autobump@users.noreply.github.com>
2022-11-18 15:14:28 +0000

    Bump version: 0.2.0.dev0 → 0.2.0

--- markata_slides/__init__.py
+++ markata_slides/__init__.py
@@ -1,6 +1,6 @@
 from markata_slides.cli import cli
 from markata_slides.slides import configure, load
 
-__version__ = "0.2.0.dev0"
+__version__ = "0.2.0"
 
 __all__ = ["load", "cli", "__version__", "configure"]
